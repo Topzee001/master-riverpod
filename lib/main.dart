9451e0094737b2d7abfd73d4d453f805a99ed980
Ibrahim Sakariyah <ISakariyah@estreamnetworks.net>
2025-06-16 12:38:53 +0100

    ex5 or so

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,85 @@ void main() {
   runApp(const ProviderScope(child: MyApp()));
 }
 
+@immutable
+class Film {
+  final String id;
+  final String title;
+  final String description;
+  final bool isFavourite;
+
+  const Film({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.isFavourite,
+  });
+  Film copy({required bool isFavourite}) {
+    return Film(
+      id: id,
+      title: title,
+      description: description,
+      isFavourite: isFavourite,
+    );
+  }
+
+  @override
+  String toString() =>
+      'Film(id: $id, title: $title, description: $description, isFavourite: $isFavourite)';
+
+  @override
+  bool operator ==(covariant Film other) {
+    return id == other.id && isFavourite == other.isFavourite;
+  }
+
+  @override
+  // TODO: implement hashCode
+  int get hashCode => Object.hashAll([id, isFavourite]);
+}
+
+const allFilms = [
+  Film(
+    id: '1',
+    title: 'Inception',
+    description: 'A mind-bending thriller about dream invasion.',
+    isFavourite: false,
+  ),
+  Film(
+    id: '2',
+    title: 'The Matrix',
+    description: 'A hacker discovers the truth about his reality.',
+    isFavourite: false,
+  ),
+  Film(
+    id: '3',
+    title: 'Interstellar',
+    description: 'Explorers travel through a wormhole in space.',
+    isFavourite: false,
+  ),
+  Film(
+    id: '4',
+    title: 'The Dark Knight',
+    description: 'Batman faces the Joker in Gotham City.',
+    isFavourite: false,
+  ),
+];
+
+class FilmNotifier extends StateNotifier<List<Film>> {
+  FilmNotifier() : super(allFilms);
+
+  void update(Film film, bool isFavourite) {
+    state =
+        state
+            .map(
+              (thisFilm) =>
+                  thisFilm.id == film.id
+                      ? thisFilm.copy(isFavourite: isFavourite)
+                      : thisFilm,
+            )
+            .toList();
+  }
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
